101c5f63294e8a296ce71450c7f285040fe368cd
Zedong Xun <zxun@wustl.edu>
2021-07-08 23:53:43 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import initTilt from "./js/tilt";
 import initSr from "./js/sr";
-import resume from "./assets/resume.pdf";
+import resume from "./assets/Resume20210415.pdf";
 
 import "./style/main.scss";
 
@@ -17,6 +17,7 @@ $('a[href^="#"]').on("click", function (event) {
   }
 });
 
+//Add resume method
 function addResume(pdf) {
   if (!pdf) return;
 
@@ -24,8 +25,34 @@ function addResume(pdf) {
   resumeButton.setAttribute("href", pdf);
 }
 
+//Autohide navigation bar when scrolling
+function autohide() {
+  const el_autohide = document.querySelector(".autohide");
+  console.log("autohide")
+
+  const navbar_height = document.querySelector(".navbar").offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+  }
+}
+
 initSr();
 initTilt();
 
 // uncomment this if you want to attach your resume
 addResume(resume);
+autohide();
