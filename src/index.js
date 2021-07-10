02ac951b76273217e7dcdfc94fd5b44ca889adb0
Zedong Xun <zxun@wustl.edu>
2021-07-10 18:38:12 +0800

    v1 complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import initTilt from "./js/tilt";
 import initSr from "./js/sr";
-import resume from "./assets/Resume20210415.pdf";
-
+import resume from "./assets/resume.pdf";
 import "./style/main.scss";
+
+// var wavesurfer = WaveSurfer.create({
+//   container: "#waveform",
+//   scrollParent: true,
+//   waveColor: "violet",
+//   progressColor: "purple",
+// });
 
 $('a[href^="#"]').on("click", function (event) {
   var target = $(this.getAttribute("href"));
@@ -28,25 +34,24 @@ function addResume(pdf) {
 //Autohide navigation bar when scrolling
 function autohide() {
   const el_autohide = document.querySelector(".autohide");
-  console.log("autohide")
+  console.log("autohide");
 
   const navbar_height = document.querySelector(".navbar").offsetHeight;
-  document.body.style.paddingTop = navbar_height + 'px';
+  document.body.style.paddingTop = navbar_height + "px";
 
-  if(el_autohide){
+  if (el_autohide) {
     var last_scroll_top = 0;
-    window.addEventListener('scroll', function() {
-          let scroll_top = window.scrollY;
-         if(scroll_top < last_scroll_top) {
-              el_autohide.classList.remove('scrolled-down');
-              el_autohide.classList.add('scrolled-up');
-          }
-          else {
-              el_autohide.classList.remove('scrolled-up');
-              el_autohide.classList.add('scrolled-down');
-          }
-          last_scroll_top = scroll_top;
-    }); 
+    window.addEventListener("scroll", function () {
+      let scroll_top = window.scrollY;
+      if (scroll_top < last_scroll_top) {
+        el_autohide.classList.remove("scrolled-down");
+        el_autohide.classList.add("scrolled-up");
+      } else {
+        el_autohide.classList.remove("scrolled-up");
+        el_autohide.classList.add("scrolled-down");
+      }
+      last_scroll_top = scroll_top;
+    });
   }
 }
 
@@ -56,3 +61,6 @@ initTilt();
 // uncomment this if you want to attach your resume
 addResume(resume);
 autohide();
+// wavesurfer.load(
+//   "http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3"
+// );
